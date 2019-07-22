556035e311b03fc014823692e46793d3339774bf
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2019-07-22 02:31:25 -0500

    ddd

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,6 +1,29 @@
 $(function() {
   var source = $("#entry-template").html(); 
   var template = Handlebars.compile(source);
+  // var kk = [
+  //   {
+  //     "cedula": "8-915-1407",
+  //     "nombre": "Taro",
+  //     "apellido": "Wook",
+  //     "posicion": "Manager",
+  //     "fecha_ingreso": "2019-01-01",
+  //     "salario": 1200,
+  //     "gastos": 300
+  //   },
+  //   {
+  //     "cedula": "8-999-999",
+  //     "nombre": "Roderiq",
+  //     "apellido": "ACB2",
+  //     "posicion": "DEV",
+  //     "fecha_ingreso": "2019-01-01",
+  //     "salario": 900,
+  //     "gastos": 100
+  //   }
+  // ];  
+  // var html = template(kk);
+  // console.log(html)
+  // $('body').append(html);
   $.ajax({
     url: "http://10.10.10.2:80/api/empleados",
     success: function(box){
@@ -9,7 +32,4 @@ $(function() {
       return box
     }
   });
-Handlebars.registerHelper('Konktenar', function(name, apellido) {
-  return name + " " + apellido;
-});
 });
