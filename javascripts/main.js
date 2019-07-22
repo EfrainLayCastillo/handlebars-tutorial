e8abed407c4cfff159dcf37fdf371c3708dd4fe7
efrainlaycastillo <efrainlaycastillo@hotmail.com>
2019-07-22 02:13:49 -0500

    s

--- javascripts/main.js
+++ javascripts/main.js
@@ -2,7 +2,7 @@ $(function() {
   var source = $("#entry-template").html(); 
   var template = Handlebars.compile(source);
   $.ajax({
-    url: "https://jsonplaceholder.typicode.com/todos",
+    url: "http://10.10.10.2:80/api/empleados",
     success: function(box){
       var html = template(box);
       $('body').append(html);
